865f5a8eedd40456d0712e6fd1f4ce1f743c4817
Marcus Nordquist <marcus@queenslab.se>
2018-10-02 19:43:22 +0200

    ♠ Init

---
+++ MvpCarExample/InternalConsole.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MvpCarExample
+{
+    partial class Program
+    {
+        internal class InternalConsole
+        {
+            internal static void Init()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.SetWindowSize(Console.LargestWindowWidth / 2, Console.LargestWindowHeight / 2);
+                Console.Clear();
+
+                Console.WriteLine(@"");
+                Console.WriteLine(@"███▄ ▄███▓ ██▒   █▓ ██▓███      ▄████▄   ▄▄▄       ██▀███  ");
+                Console.WriteLine(@"▓██▒▀█▀ ██▒▓██░   █▒▓██░  ██▒   ▒██▀ ▀█  ▒████▄    ▓██ ▒ ██▒");
+                Console.WriteLine(@"▓██    ▓██░ ▓██  █▒░▓██░ ██▓▒   ▒▓█    ▄ ▒██  ▀█▄  ▓██ ░▄█ ▒");
+                Console.WriteLine(@"▒██    ▒██   ▒██ █░░▒██▄█▓▒ ▒   ▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██▀▀█▄  ");
+                Console.WriteLine(@"▒██▒   ░██▒   ▒▀█░  ▒██▒ ░  ░   ▒ ▓███▀ ░ ▓█   ▓██▒░██▓ ▒██▒");
+                Console.WriteLine(@"░ ▒░   ░  ░   ░ ▐░  ▒▓▒░ ░  ░   ░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒▓ ░▒▓░");
+                Console.WriteLine(@"░  ░      ░   ░ ░░  ░▒ ░          ░  ▒     ▒   ▒▒ ░  ░▒ ░ ▒░");
+                Console.WriteLine(@"░      ░        ░░  ░░          ░          ░   ▒     ░░   ░ ");
+                Console.WriteLine(@"       ░         ░              ░ ░            ░  ░   ░     ");
+                Console.WriteLine(@"                ░               ░                           ");
+                Console.WriteLine();
+                Console.WriteLine($"Advanced OOP - Class of 2018 :]");
+                Console.WriteLine("                                                   Type quit to exit.");
+                Console.WriteLine("");
+
+                Console.WriteLine("Type: 'car command <-param value>' to interact, example: car turn -degrees 45, car forward -speed 10, car reverse -speed 10)");
+                Console.WriteLine("Type: 'car status' to describe where the Car is now and where it is headed");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ MvpCarExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MvpCarExample.Domain;
+
+namespace MvpCarExample
+{
+    partial class Program
+    {
+        private static string _consoleChars = "MyCarMvp>";
+        private static readonly Car Car = new Car(0,0);
+
+        static void Main(string[] args)
+        {
+            InternalConsole.Init();
+
+            while (true)
+            {
+                Console.Write(_consoleChars);
+                var line = Console.ReadLine();
+
+                if (line == null)
+                    continue;
+
+                if (!string.IsNullOrWhiteSpace(line) && line.Trim().Equals("quit", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                if (!string.IsNullOrWhiteSpace(line) && line.Trim().Equals("cls", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                if (line.Trim().Equals("car status", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"");
+                    Console.WriteLine($"CAR STATUS");
+                    Console.WriteLine($"Angle: {Car.Angle}");
+                    Console.WriteLine($"Speed: {Car.Speed}");
+                    Console.WriteLine($"X: {Car.X}");
+                    Console.WriteLine($"X: {Car.Y}");
+                    Console.WriteLine($"");
+                }
+
+            }
+        }
+    }
+}
