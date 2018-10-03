42f185e1382590d1544d4295bbbf90dfd6ba22f5
Marcus Nordquist <marcus@queenslab.se>
2018-10-03 11:27:40 +0200

    ♠ Fix, car

--- MvpCarExample.Domain/Car.cs
+++ MvpCarExample.Domain/Car.cs
@@ -2,42 +2,19 @@
 
 namespace MvpCarExample.Domain
 {
-    public class Vector
-    {
-        public double X { get; set; }
-        public double Y { get; set; }
-    }
-
     public class Car
     {
         public float Angle { get; private set; }
         public int Speed { get; private set; }
-        public Vector Position { get; private set; }
-
-        public void Move(float angle)
-        {
-            var radians = DegreeToRadian(angle);
-            Position.X += Speed * Math.Cos(radians);
-            Position.Y += Speed * Math.Sin(radians);
-            Speed--;
-        }
-
-        public void Throttle(int throttle)
-        {
-            Speed += throttle;
-        }
+        public int X { get; private set; }
+        public int Y { get; private set; }
 
         public Car(int x, int y)
         {
-            Position = new Vector();
-
+            X = x;
+            Y = y;
             Angle = 0;
-            Speed = 10;
-        }
-
-        private double DegreeToRadian(double angle)
-        {
-            return Math.PI * angle / 180.0;
+            Speed = 0;
         }
     }
 }
