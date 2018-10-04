44b890397aa455207d34f5b899461350dcb6934d
StankovicMarko89 <43344688+StankovicMarko89@users.noreply.github.com>
2018-10-04 13:11:43 +0200

    Grupp 3

--- MvpCarExample.Domain/Car.cs
+++ MvpCarExample.Domain/Car.cs
@@ -5,9 +5,11 @@ namespace MvpCarExample.Domain
     public class Car
     {
         public float Angle { get; private set; }
-        public int Speed { get; private set; }
+        
         public int X { get; private set; }
         public int Y { get; private set; }
+        public int Speed { get; private set; }
+        public bool EngineOn { get; private set; }
 
         public Car(int x, int y)
         {
@@ -16,5 +18,32 @@ namespace MvpCarExample.Domain
             Angle = 0;
             Speed = 0;
         }
+
+        public void Accelarate()
+        {
+            this.Speed = this.Speed + 1;
+        }
+        public void Break()
+        {
+            this.Speed = this.Speed - 1;
+        }
+
+        public void Move(float _angel)
+        {
+            this.Angle = _angel;
+            this.X += this.Speed * Convert.ToInt32(this.Angle);
+            this.Y += this.Speed * Convert.ToInt32(this.Angle);
+
+        }
+        public void TurnOn()
+        {
+            this.EngineOn = true;
+        }
+        public void TurnOff()
+        {
+            this.EngineOn = false;
+        }
     }
+
+
 }
--- MvpCarExample/Program.cs
+++ MvpCarExample/Program.cs
@@ -7,9 +7,11 @@ namespace MvpCarExample
     {
         private static string _consoleChars = "MyCarMvp>";
         private static readonly Car Car = new Car(0,0);
+        
 
         static void Main(string[] args)
         {
+            
             InternalConsole.Init();
 
             while (true)
@@ -33,13 +35,54 @@ namespace MvpCarExample
                 {
                     Console.WriteLine($"");
                     Console.WriteLine($"CAR STATUS");
+                    Console.WriteLine($"EngineOn: {Car.EngineOn}");
                     Console.WriteLine($"Angle: {Car.Angle}");
                     Console.WriteLine($"Speed: {Car.Speed}");
                     Console.WriteLine($"X: {Car.X}");
-                    Console.WriteLine($"X: {Car.Y}");
+                    Console.WriteLine($"Y: {Car.Y}");
                     Console.WriteLine($"");
                 }
 
+                if (line.Trim().Equals("car start", StringComparison.OrdinalIgnoreCase))
+                {
+                    Car.TurnOn();
+                }
+                if (line.Trim().Equals("car help", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("cmd you can use in this game");
+                    Console.WriteLine("car start");
+                    Console.WriteLine("car stop");
+                    Console.WriteLine("car acc");
+                    Console.WriteLine("car break");
+                    Console.WriteLine("car move");
+                }
+
+                if (Car.EngineOn == false)
+                {
+                    Console.WriteLine("Engine is not on");
+                }
+                else
+                {
+                   
+                    if (line.Trim().Equals("car stop", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Car.TurnOff();
+                    }
+                    if (line.Trim().Equals("car acc", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Car.Accelarate();
+                    }
+                    if (line.Trim().Equals("car break", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Car.Break();
+                    }
+                    if (line.Trim().Equals("car move", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("enter angle:");
+
+                        Car.Move(float.Parse(Console.ReadLine()));
+                    }
+                }
             }
         }
     }
